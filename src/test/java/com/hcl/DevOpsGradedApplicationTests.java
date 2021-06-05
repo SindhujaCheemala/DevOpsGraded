a1340ea8e65edcb47dcef101e6c32a8ae7cb67b3
SindhujaCheemala <sindhujacheemala@gmail.com>
2021-06-05 11:31:31 +0530

    <dependency>

---
+++ src/test/java/com/hcl/DevOpsGradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsGradedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
